1f1c4e5b53063e6f6e91af5b152133f8f1aae6f2
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2014-12-18 23:21:51 +0100

    Added a semi-working navigation drawer with class/teacher/classroom display lists

--- app/src/main/java/com/liquid/kochanowski/KochanowskiMainActivity.java
+++ app/src/main/java/com/liquid/kochanowski/KochanowskiMainActivity.java
@@ -1,29 +1,37 @@
 package com.liquid.kochanowski;
 
 import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.liquid.kochanparser.TimeTableType;
 
+import org.lucasr.twowayview.ItemClickSupport;
+import org.lucasr.twowayview.TwoWayLayoutManager;
+import org.lucasr.twowayview.widget.ListLayoutManager;
+
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 
-public class KochanowskiMainActivity extends ActionBarActivity implements AdapterView.OnItemSelectedListener
+public class KochanowskiMainActivity extends ActionBarActivity implements AdapterView.OnItemSelectedListener, TimeTableListFragment.OnTimeTableSelectedListener
 {
     private static TimeTableDbHelper helper;
 
@@ -33,9 +41,74 @@ public class KochanowskiMainActivity extends ActionBarActivity implements Adapte
     private Spinner spinner;
 
     private DrawerLayout drawerLayout;
+
+    private RecyclerView drawerList;
+    private RecyclerView.LayoutManager drawerLayoutManager;
+    private NavDrawerAdapter drawerAdapter;
+    private ItemClickSupport clickSupport;
+
     private ActionBarDrawerToggle toggle;
 
     private TimeTableDisplayFragment fragment;
+
+    private static final int SCREEN_TODAY = 0;
+    private static final int SCREEN_CLASSES = 1;
+    private static final int SCREEN_TEACHERS = 2;
+    private static final int SCREEN_CLASSROOMS = 3;
+    private static final int SCREEN_DISPLAY = 4;
+
+    private List <String> values = new ArrayList <> ();
+    private List <Integer> icons = new ArrayList <> ();
+
+    private class NavDrawerAdapter extends RecyclerView.Adapter<NavDrawerAdapter.DrawerViewHolder>
+    {
+        private List <String> values;
+        private List <Integer> icons;
+
+        private int resource;
+
+        public class DrawerViewHolder extends RecyclerView.ViewHolder
+        {
+            TextView name;
+            ImageView icon;
+
+            public DrawerViewHolder (View v)
+            {
+                super (v);
+
+                name = (TextView) v.findViewById (R.id.text);
+                icon = (ImageView) v.findViewById (R.id.item_icon);
+            }
+        }
+
+        public NavDrawerAdapter (int resource, List <String> values, List<Integer> icons)
+        {
+            this.resource = resource;
+            this.values = values;
+            this.icons = icons;
+        }
+
+        @Override
+        public DrawerViewHolder onCreateViewHolder (ViewGroup parent, int viewType)
+        {
+            View v = LayoutInflater.from (parent.getContext ()).inflate (resource, parent, false);
+
+            return new DrawerViewHolder (v);
+        }
+
+        @Override
+        public void onBindViewHolder (DrawerViewHolder holder, int position)
+        {
+            holder.name.setText (values.get (position));
+            holder.icon.setImageDrawable (getResources ().getDrawable (icons.get (position)));
+        }
+
+        @Override
+        public int getItemCount ()
+        {
+            return values.size ();
+        }
+    }
 
     public KochanowskiMainActivity ()
     {
@@ -48,10 +121,25 @@ public class KochanowskiMainActivity extends ActionBarActivity implements Adapte
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_kochanowski_main);
 
+        values.add (getString (R.string.day_name_today));
+        values.add (getString (R.string.classes));
+        values.add (getString (R.string.teachers));
+        values.add (getString (R.string.classrooms));
+
+        icons.add (R.drawable.ic_person_black);
+        icons.add (R.drawable.ic_group_black);
+        icons.add (R.drawable.ic_person_black);
+        icons.add (R.drawable.ic_person_black);
+
         toolbar = (Toolbar) findViewById (R.id.activity_main_toolbar);
         spinner = (Spinner) findViewById (R.id.main_activity_spinner);
 
         drawerLayout = (DrawerLayout) findViewById (R.id.drawer_layout);
+
+        drawerList = (RecyclerView) findViewById (R.id.left_drawer);
+        drawerLayoutManager = new ListLayoutManager (this, TwoWayLayoutManager.Orientation.VERTICAL);
+        drawerAdapter = new NavDrawerAdapter (R.layout.drawer_list_item, values, icons);
+        clickSupport = ItemClickSupport.addTo (drawerList);
 
         toggle = new ActionBarDrawerToggle (
                 this,
@@ -64,10 +152,18 @@ public class KochanowskiMainActivity extends ActionBarActivity implements Adapte
         prefs = getSharedPreferences (getString (R.string.shared_prefs_name), MODE_PRIVATE);
     }
 
-    @Override
-    protected void onResume ()
+    protected void onPostCreate (Bundle savedInstanceState)
     {
-        super.onResume ();
+        super.onPostCreate (savedInstanceState);
+
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        toggle.syncState ();
+    }
+
+    @Override
+    protected void onStart ()
+    {
+        super.onStart ();
 
         if (toolbar != null)
         {
@@ -77,31 +173,44 @@ public class KochanowskiMainActivity extends ActionBarActivity implements Adapte
 
             drawerLayout.setDrawerListener (toggle);
             drawerLayout.setStatusBarBackgroundColor (getResources ().getColor (android.R.color.darker_gray));
+
+            drawerList.setLayoutManager (drawerLayoutManager);
+            drawerList.setAdapter (drawerAdapter);
+            setUpClickListeners ();
         }
 
         DaySelectAdapter adapter = new DaySelectAdapter (this, R.layout.spinner_item, DaySelectAdapter.getDays (this));
         spinner.setAdapter (adapter);
         spinner.setOnItemSelectedListener (this);
         spinner.setSelection (Calendar.getInstance ().get (Calendar.DAY_OF_WEEK) - 2);
+        spinner.setVisibility (View.VISIBLE);
 
         fragment = TimeTableDisplayFragment.newInstance (
                 prefs.getString (getString (R.string.pref_table_name), ""),
-                TimeTableType.TIMETABLE_TYPE_CLASS,
+                TimeTableType.CLASS,
                 1);
 
         FragmentTransaction transaction = getFragmentManager ().beginTransaction ();
 
         transaction.replace (R.id.fragmentStub, fragment);
         transaction.commit ();
+
+        Log.i ("liquid", "onStart ()");
     }
 
     @Override
-    protected void onPostCreate (Bundle savedInstanceState)
+    protected void onResume ()
     {
-        super.onPostCreate (savedInstanceState);
+        super.onResume ();
 
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        toggle.syncState ();
+        Log.i ("liquid", "onResume ()");
+    }
+
+    @Override
+    protected void onSaveInstanceState (Bundle outState)
+    {
+
+        super.onSaveInstanceState (outState);
     }
 
     @Override
@@ -148,6 +257,7 @@ public class KochanowskiMainActivity extends ActionBarActivity implements Adapte
         Log.i ("liquid", "Item selected: " + position);
 
         fragment.setDay (position);
+        fragment.refresh ();
     }
 
     @Override
@@ -159,5 +269,76 @@ public class KochanowskiMainActivity extends ActionBarActivity implements Adapte
     public static TimeTableDbHelper getHelper ()
     {
         return helper;
+    }
+
+    @Override
+    public void onTimeTableSelected (String tableName, int tableType)
+    {
+        Log.i ("liquid", "Selected " + tableName);
+
+        /*TimeTableDisplayFragment fragment = TimeTableDisplayFragment.newInstance (tableName, tableType, 0);
+        fragment.setDay (0);
+
+        FragmentTransaction transaction = getFragmentManager ().beginTransaction ();
+        transaction.replace (R.id.fragmentStub, fragment);
+        transaction.commit ();*/
+    }
+
+    private void setUpClickListeners ()
+    {
+        clickSupport.setOnItemClickListener (new ItemClickSupport.OnItemClickListener ()
+        {
+            @Override
+            public void onItemClick (RecyclerView recyclerView, View view, int position, long id)
+            {
+                int tableType = TimeTableType.NONE;
+                switch (position)
+                {
+                    case SCREEN_TODAY:
+                        tableType = TimeTableType.NONE;
+                        break;
+                    case SCREEN_CLASSES:
+                        tableType = TimeTableType.CLASS;
+                        break;
+                    case SCREEN_TEACHERS:
+                        tableType = TimeTableType.TEACHER;
+                        break;
+                    case SCREEN_CLASSROOMS:
+                        tableType = TimeTableType.CLASSROOM;
+                        break;
+                }
+
+                FragmentTransaction transaction = getFragmentManager ().beginTransaction ();
+
+                if (tableType != TimeTableType.NONE)
+                {
+                    TimeTableListFragment fragment = TimeTableListFragment.newInstance (tableType);
+
+                    spinner.setVisibility (View.GONE);
+                    getSupportActionBar ().setDisplayShowTitleEnabled (true);
+                    getSupportActionBar ().setTitle (values.get (position));
+
+                    transaction.replace (R.id.fragmentStub, fragment);
+                }
+                else
+                {
+                    getSupportActionBar ().setDisplayShowTitleEnabled (false);
+                    spinner.setVisibility (View.VISIBLE);
+
+                    int currentDay = Calendar.getInstance ().get (Calendar.DAY_OF_WEEK) - 2;
+
+                    if (currentDay == spinner.getSelectedItemPosition ()) fragment.setDay (currentDay);
+                    else spinner.setSelection (currentDay);
+
+                    transaction.replace (R.id.fragmentStub, fragment);
+                }
+
+                transaction.commit ();
+
+                drawerLayout.closeDrawer (drawerList);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/liquid/kochanowski/TimeTableListFragment.java
@@ -0,0 +1,229 @@
+package com.liquid.kochanowski;
+
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.liquid.kochanowski.TimeTableContract.*;
+import com.liquid.kochanparser.TimeTableType;
+
+import org.lucasr.twowayview.ItemClickSupport;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link TimeTableListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TimeTableListFragment extends Fragment
+{
+    public class TimeTableListAdapter extends RecyclerView.Adapter<TimeTableListAdapter.ListViewHolder>
+    {
+        private Cursor cur;
+        private SQLiteDatabase db;
+
+        private int tableType;
+
+        public class ListViewHolder extends RecyclerView.ViewHolder
+        {
+            TextView name;
+
+            public ListViewHolder (View v)
+            {
+                super (v);
+
+                name = (TextView) v.findViewById (R.id.timetable_name);
+            }
+        }
+
+        public TimeTableListAdapter (int tableType)
+        {
+            db = KochanowskiMainActivity.getHelper ().getReadableDatabase ();
+            this.tableType = tableType;
+
+            switch (tableType)
+            {
+                case TimeTableType.CLASS:
+                    cur = db.rawQuery ("SELECT * FROM classes ORDER BY longname ASC", null);
+                    break;
+                case TimeTableType.TEACHER:
+                    cur = db.rawQuery ("SELECT * FROM teachers ORDER BY code ASC" ,null);
+                    break;
+                case TimeTableType.CLASSROOM:
+                    cur = db.rawQuery ("SELECT DISTINCT classroom FROM lessons ORDER BY classroom ASC", null);
+                    break;
+            }
+
+        }
+
+        @Override
+        public ListViewHolder onCreateViewHolder (ViewGroup parent, int viewType)
+        {
+            View v = LayoutInflater.from (parent.getContext ()).inflate (R.layout.tablelist_item, parent, false);
+
+            return new ListViewHolder (v);
+        }
+
+        @Override
+        public void onBindViewHolder (ListViewHolder holder, int position)
+        {
+            cur.moveToPosition (position);
+
+            switch (tableType)
+            {
+                case TimeTableType.CLASS:
+                    String shortname = cur.getString (cur.getColumnIndexOrThrow (ClassTable.COLUMN_NAME_NAME_SHORT));
+                    String longname = cur.getString (cur.getColumnIndexOrThrow (ClassTable.COLUMN_NAME_NAME_LONG));
+
+                    holder.name.setText (longname + " (" + shortname + ")");
+                    break;
+                case TimeTableType.TEACHER:
+                    String name = cur.getString (cur.getColumnIndexOrThrow (TeacherTable.COLUMN_NAME_TEACHER_NAME));
+                    String surname = cur.getString (cur.getColumnIndexOrThrow (TeacherTable.COLUMN_NAME_TEACHER_SURNAME));
+                    String code = cur.getString (cur.getColumnIndexOrThrow (TeacherTable.COLUMN_NAME_TEACHER_CODE));
+
+                    holder.name.setText (name + " " + surname + " (" + code + ")");
+                    break;
+                case TimeTableType.CLASSROOM:
+                    String classroomName = cur.getString (cur.getColumnIndexOrThrow (LessonTable.COLUMN_NAME_CLASSROOM));
+
+                    holder.name.setText (classroomName);
+                    break;
+            }
+        }
+
+        @Override
+        public int getItemCount ()
+        {
+            return cur.getCount ();
+        }
+
+        public Cursor getCursor ()
+        {
+            return cur;
+        }
+    }
+
+    public interface OnTimeTableSelectedListener
+    {
+        public void onTimeTableSelected (String tableName, int tableType);
+    }
+
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_TYPE = "type";
+
+    private int tableType;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private ItemClickSupport clickSupport;
+
+    private OnTimeTableSelectedListener listener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param tableType
+     * @return A new instance of fragment TimeTableListFragment.
+     */
+    public static TimeTableListFragment newInstance (int tableType)
+    {
+        TimeTableListFragment fragment = new TimeTableListFragment ();
+        Bundle args = new Bundle ();
+
+        args.putInt (ARG_TYPE, tableType);
+
+        fragment.setArguments (args);
+        return fragment;
+    }
+
+    public TimeTableListFragment ()
+    {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach (Activity activity)
+    {
+        super.onAttach (activity);
+
+        try
+        {
+            listener = (OnTimeTableSelectedListener) activity;
+        }
+        catch (ClassCastException e)
+        {
+            throw new ClassCastException (activity.toString () + " must implement OnTimeTableSelectListener");
+        }
+    }
+
+    @Override
+    public void onCreate (Bundle savedInstanceState)
+    {
+        super.onCreate (savedInstanceState);
+
+        if (getArguments () != null)
+        {
+            tableType = getArguments ().getInt (ARG_TYPE);
+        }
+    }
+
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate (R.layout.fragment_time_table_list, container, false);
+
+        recyclerView = (RecyclerView) v.findViewById (R.id.timetable_list);
+        layoutManager = new LinearLayoutManager (this.getActivity ());
+        recyclerView.setLayoutManager (layoutManager);
+
+        adapter = new TimeTableListAdapter (tableType);
+
+        recyclerView.setAdapter (adapter);
+
+        clickSupport = ItemClickSupport.addTo (recyclerView);
+
+        clickSupport.setOnItemClickListener (new ItemClickSupport.OnItemClickListener ()
+        {
+            @Override
+            public void onItemClick (RecyclerView recyclerView, View view, int position, long id)
+            {
+                String tableName = "";
+
+                Cursor cur = ((TimeTableListAdapter) adapter).getCursor ();
+                cur.moveToPosition (position);
+
+                switch (tableType)
+                {
+                    case TimeTableType.CLASS:
+                        tableName = cur.getString (cur.getColumnIndexOrThrow (LessonTable.COLUMN_NAME_CLASS_NAME_SHORT));
+                        break;
+                    case TimeTableType.TEACHER:
+                        tableName = cur.getString (cur.getColumnIndexOrThrow (TeacherTable.COLUMN_NAME_TEACHER_CODE));
+                        break;
+                    case TimeTableType.CLASSROOM:
+                        tableName = cur.getString (cur.getColumnIndexOrThrow (LessonTable.COLUMN_NAME_CLASSROOM));
+                        break;
+                }
+
+                listener.onTimeTableSelected (tableName, tableType);
+            }
+        });
+
+        return v;
+    }
+}
