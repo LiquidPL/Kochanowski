54ec6450619c16efda661a8034ef458a87529724
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2014-12-20 17:00:13 +0100

    Rewrote main activity for proper handling of activity lifecycle

--- app/src/main/java/com/liquid/kochanowski/KochanowskiMainActivity.java
+++ app/src/main/java/com/liquid/kochanowski/KochanowskiMainActivity.java
@@ -49,13 +49,24 @@ public class KochanowskiMainActivity extends ActionBarActivity implements Adapte
 
     private ActionBarDrawerToggle toggle;
 
-    private TimeTableDisplayFragment fragment;
+    private TimeTableDisplayFragment displayFragment;
+    private TimeTableListFragment listFragment;
 
     private static final int SCREEN_TODAY = 0;
     private static final int SCREEN_CLASSES = 1;
     private static final int SCREEN_TEACHERS = 2;
     private static final int SCREEN_CLASSROOMS = 3;
     private static final int SCREEN_DISPLAY = 4;
+
+    private static final String ARG_SCREEN = "screen";
+    private static final String ARG_TABLE = "table";
+    private static final String ARG_DAY = "day";
+    private static final String ARG_TYPE = "type";
+
+    private int currentScreen = -1;
+    private String currentTable = "";
+    private int currentDay = -1;
+    private int currentType = -1;
 
     private List <String> values = new ArrayList <> ();
     private List <Integer> icons = new ArrayList <> ();
@@ -150,6 +161,21 @@ public class KochanowskiMainActivity extends ActionBarActivity implements Adapte
         );
 
         prefs = getSharedPreferences (getString (R.string.shared_prefs_name), MODE_PRIVATE);
+
+        if (savedInstanceState != null)
+        {
+            currentScreen = savedInstanceState.getInt (ARG_SCREEN);
+            currentTable = savedInstanceState.getString (ARG_TABLE);
+            currentDay = savedInstanceState.getInt (ARG_DAY);
+            currentType = savedInstanceState.getInt (ARG_TYPE);
+        }
+        else
+        {
+            currentScreen = SCREEN_TODAY;
+            currentTable = prefs.getString (getString (R.string.pref_table_name), "");
+            currentDay = getCurrentDay ();
+            currentType = TimeTableType.CLASS;
+        }
     }
 
     protected void onPostCreate (Bundle savedInstanceState)
@@ -161,47 +187,50 @@ public class KochanowskiMainActivity extends ActionBarActivity implements Adapte
     }
 
     @Override
-    protected void onStart ()
+    protected void onResume ()
     {
-        super.onStart ();
+        super.onResume ();
 
         if (toolbar != null)
         {
             setSupportActionBar (toolbar);
             toolbar.setNavigationIcon (R.drawable.ic_menu_black);
-            getSupportActionBar ().setDisplayShowTitleEnabled (false);
 
             drawerLayout.setDrawerListener (toggle);
             drawerLayout.setStatusBarBackgroundColor (getResources ().getColor (android.R.color.darker_gray));
 
             drawerList.setLayoutManager (drawerLayoutManager);
             drawerList.setAdapter (drawerAdapter);
-            setUpClickListeners ();
+
+            clickSupport.setOnItemClickListener (new ItemClickSupport.OnItemClickListener ()
+            {
+                @Override
+                public void onItemClick (RecyclerView recyclerView, View view, int position, long id)
+                {
+                    switch (position)
+                    {
+                        case SCREEN_TODAY:
+                            selectScreen (SCREEN_TODAY, prefs.getString (getString (R.string.pref_table_name), ""), getCurrentDay (), TimeTableType.CLASS);
+                            break;
+                        case SCREEN_CLASSES:
+                            selectScreen (SCREEN_CLASSES, "", -1, -1);
+                            break;
+                        case SCREEN_TEACHERS:
+                            selectScreen (SCREEN_TEACHERS, "", -1, -1);
+                            break;
+                        case SCREEN_CLASSROOMS:
+                            selectScreen (SCREEN_CLASSROOMS, "", -1, -1);
+                            break;
+                    }
+                }
+            });
         }
 
         DaySelectAdapter adapter = new DaySelectAdapter (this, R.layout.spinner_item, DaySelectAdapter.getDays (this));
         spinner.setAdapter (adapter);
         spinner.setOnItemSelectedListener (this);
-        spinner.setSelection (Calendar.getInstance ().get (Calendar.DAY_OF_WEEK) - 2);
-        spinner.setVisibility (View.VISIBLE);
 
-        fragment = TimeTableDisplayFragment.newInstance (
-                prefs.getString (getString (R.string.pref_table_name), ""),
-                TimeTableType.CLASS,
-                1);
-
-        FragmentTransaction transaction = getFragmentManager ().beginTransaction ();
-
-        transaction.replace (R.id.fragmentStub, fragment);
-        transaction.commit ();
-
-        Log.i ("liquid", "onStart ()");
-    }
-
-    @Override
-    protected void onResume ()
-    {
-        super.onResume ();
+        selectScreen (currentScreen, currentTable, currentDay, currentType);
 
         Log.i ("liquid", "onResume ()");
     }
@@ -209,6 +238,10 @@ public class KochanowskiMainActivity extends ActionBarActivity implements Adapte
     @Override
     protected void onSaveInstanceState (Bundle outState)
     {
+        outState.putInt (ARG_SCREEN, currentScreen);
+        outState.putString (ARG_TABLE, currentTable);
+        outState.putInt (ARG_DAY, currentDay);
+        outState.putInt (ARG_TYPE, currentType);
 
         super.onSaveInstanceState (outState);
     }
@@ -251,19 +284,63 @@ public class KochanowskiMainActivity extends ActionBarActivity implements Adapte
         return super.onOptionsItemSelected (item);
     }
 
-    @Override
-    public void onItemSelected (AdapterView<?> parent, View view, int position, long id)
+    private void selectScreen (int screenId, String table, int day, int type)
     {
-        Log.i ("liquid", "Item selected: " + position);
+        currentScreen = screenId;
+        currentTable = table;
+        currentDay = day;
+        currentType = type;
 
-        fragment.setDay (position);
-        fragment.refresh ();
+        FragmentTransaction transaction = getFragmentManager ().beginTransaction ();
+
+        switch (currentScreen)
+        {
+            case SCREEN_TODAY:
+                displayFragment = TimeTableDisplayFragment.newInstance (currentTable, currentType, currentDay, 0);
+                transaction.replace (R.id.fragment_stub, displayFragment);
+
+                getSupportActionBar ().setDisplayShowTitleEnabled (false);
+                spinner.setVisibility (View.VISIBLE);
+
+                spinner.setSelection (currentDay);
+                break;
+            case SCREEN_CLASSES:
+                listFragment = TimeTableListFragment.newInstance (TimeTableType.CLASS);
+                transaction.replace (R.id.fragment_stub, listFragment);
+
+                getSupportActionBar ().setDisplayShowTitleEnabled (true);
+                getSupportActionBar ().setTitle (getString (R.string.classes));
+                spinner.setVisibility (View.GONE);
+                break;
+            case SCREEN_TEACHERS:
+                listFragment = TimeTableListFragment.newInstance (TimeTableType.TEACHER);
+                transaction.replace (R.id.fragment_stub, listFragment);
+
+                getSupportActionBar ().setDisplayShowTitleEnabled (true);
+                getSupportActionBar ().setTitle (getString (R.string.teachers));
+                spinner.setVisibility (View.GONE);
+                break;
+            case SCREEN_CLASSROOMS:
+                listFragment = TimeTableListFragment.newInstance (TimeTableType.CLASSROOM);
+                transaction.replace (R.id.fragment_stub, listFragment);
+
+                getSupportActionBar ().setDisplayShowTitleEnabled (true);
+                getSupportActionBar ().setTitle (getString (R.string.classrooms));
+                spinner.setVisibility (View.GONE);
+                break;
+            case SCREEN_DISPLAY:
+                break;
+        }
+        transaction.commit ();
+
+        drawerLayout.closeDrawer (drawerList);
     }
 
-    @Override
-    public void onNothingSelected (AdapterView<?> parent)
+    private int getCurrentDay ()
     {
-
+        int day = Calendar.getInstance ().get (Calendar.DAY_OF_WEEK) - 2;
+        if (day > 4) day = 0;
+        return day;
     }
 
     public static TimeTableDbHelper getHelper ()
@@ -272,73 +349,25 @@ public class KochanowskiMainActivity extends ActionBarActivity implements Adapte
     }
 
     @Override
-    public void onTimeTableSelected (String tableName, int tableType)
+    public void onItemSelected (AdapterView<?> parent, View view, int position, long id)
     {
-        Log.i ("liquid", "Selected " + tableName);
-
-        /*TimeTableDisplayFragment fragment = TimeTableDisplayFragment.newInstance (tableName, tableType, 0);
-        fragment.setDay (0);
-
-        FragmentTransaction transaction = getFragmentManager ().beginTransaction ();
-        transaction.replace (R.id.fragmentStub, fragment);
-        transaction.commit ();*/
+        if (currentScreen == SCREEN_TODAY)
+        {
+            currentDay = position;
+            displayFragment.setDay (position);
+            displayFragment.refresh ();
+        }
     }
 
-    private void setUpClickListeners ()
+    @Override
+    public void onNothingSelected (AdapterView<?> parent)
     {
-        clickSupport.setOnItemClickListener (new ItemClickSupport.OnItemClickListener ()
-        {
-            @Override
-            public void onItemClick (RecyclerView recyclerView, View view, int position, long id)
-            {
-                int tableType = TimeTableType.NONE;
-                switch (position)
-                {
-                    case SCREEN_TODAY:
-                        tableType = TimeTableType.NONE;
-                        break;
-                    case SCREEN_CLASSES:
-                        tableType = TimeTableType.CLASS;
-                        break;
-                    case SCREEN_TEACHERS:
-                        tableType = TimeTableType.TEACHER;
-                        break;
-                    case SCREEN_CLASSROOMS:
-                        tableType = TimeTableType.CLASSROOM;
-                        break;
-                }
 
-                FragmentTransaction transaction = getFragmentManager ().beginTransaction ();
+    }
 
-                if (tableType != TimeTableType.NONE)
-                {
-                    TimeTableListFragment fragment = TimeTableListFragment.newInstance (tableType);
-
-                    spinner.setVisibility (View.GONE);
-                    getSupportActionBar ().setDisplayShowTitleEnabled (true);
-                    getSupportActionBar ().setTitle (values.get (position));
-
-                    transaction.replace (R.id.fragmentStub, fragment);
-                }
-                else
-                {
-                    getSupportActionBar ().setDisplayShowTitleEnabled (false);
-                    spinner.setVisibility (View.VISIBLE);
-
-                    int currentDay = Calendar.getInstance ().get (Calendar.DAY_OF_WEEK) - 2;
-
-                    if (currentDay == spinner.getSelectedItemPosition ()) fragment.setDay (currentDay);
-                    else spinner.setSelection (currentDay);
-
-                    transaction.replace (R.id.fragmentStub, fragment);
-                }
-
-                transaction.commit ();
-
-                drawerLayout.closeDrawer (drawerList);
-            }
-        });
-
+    @Override
+    public void onTimeTableSelected (String tableName, int tableType)
+    {
 
     }
 }
