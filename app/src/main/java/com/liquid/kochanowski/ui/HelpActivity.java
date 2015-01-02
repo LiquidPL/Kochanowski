3c721fefb616876aeb6f1bdba0a19a2ecf68e311
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2015-01-02 17:37:30 +0100

    Added a 'open source licenses' dialog to Help

--- app/src/main/java/com/liquid/kochanowski/ui/HelpActivity.java
+++ app/src/main/java/com/liquid/kochanowski/ui/HelpActivity.java
@@ -2,10 +2,11 @@ package com.liquid.kochanowski.ui;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.os.Build;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +15,11 @@ import android.widget.TextView;
 
 import com.liquid.kochanowski.BuildConfig;
 import com.liquid.kochanowski.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class HelpActivity extends BaseActivity
 {
@@ -44,6 +50,53 @@ public class HelpActivity extends BaseActivity
             builder.setPositiveButton (getString(R.string.action_ok), null);
 
             // return the dialog
+            return builder.create ();
+        }
+    }
+
+    public static class LicensesDialogFragment extends DialogFragment
+    {
+        @NonNull
+        @Override
+        public Dialog onCreateDialog (Bundle savedInstanceState)
+        {
+            // get the dialog builder and layout inflater
+            AlertDialog.Builder builder = new AlertDialog.Builder (getActivity ());
+            LayoutInflater inflater = getActivity ().getLayoutInflater ();
+
+            // inflate the view
+            View view = inflater.inflate (R.layout.dialog_licenses, null);
+
+            // put the licenses file into a TextView
+            AssetManager manager = getActivity ().getAssets ();
+            try
+            {
+                InputStream istr = manager.open ("licenses.txt");
+                BufferedReader reader = new BufferedReader (new InputStreamReader (istr));
+
+                StringBuilder stringBuilder = new StringBuilder ();
+                String input;
+
+                while ((input = reader.readLine ()) != null)
+                {
+                    stringBuilder.append (input);
+                    stringBuilder.append ("\n");
+                }
+
+                TextView textView = (TextView) view.findViewById (R.id.licenses_text);
+                textView.setText (stringBuilder.toString ());
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace ();
+            }
+
+            builder.setView (view);
+
+            builder.setTitle (getString (R.string.action_os_licenses));
+
+            builder.setPositiveButton (getString (R.string.action_ok), null);
+
             return builder.create ();
         }
     }
@@ -82,9 +135,10 @@ public class HelpActivity extends BaseActivity
         {
             case R.id.action_about:
                 new AboutDialogFragment ().show (getSupportFragmentManager (), null);
-                break;
+                return true;
             case R.id.action_os_licenses:
-                break;
+                new LicensesDialogFragment ().show (getSupportFragmentManager (), null);
+                return true;
         }
 
         return super.onOptionsItemSelected (item);
